1d1fbde7ec05373092e73eea7c59cb75965df139
Medo1902 <C20338661@mytudublin.ie>
2024-04-23 10:46:42 +0100

    basic gizmos drawing, size and lenght

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -1,19 +1,23 @@
 @tool
 extends Node3D
 
-@export var lenght = 0.0
-@export var frequency = 0.0
-@export var start_angle = 0.0
-@export var base_size = 0.0
-@export var multiplier = 0.0
+@export var length = 20
+@export var frequency = 1
+@export var start_angle = 1
+@export var base_size = 1
+@export var multiplier = 1
 
 func _process(delta):
-	pass		
+	var box_size = Vector3(base_size, base_size, base_size)  # Size of each box
+	for i in range(length):
+		var position = global_transform.origin + -global_transform.basis.x * base_size * i 
+		DebugDraw3D.draw_box(position, Quaternion.IDENTITY, box_size, Color.CYAN)
+
 
 func _ready():
 	if not Engine.is_editor_hint():		
 		pass
-	
+
 
 
 
