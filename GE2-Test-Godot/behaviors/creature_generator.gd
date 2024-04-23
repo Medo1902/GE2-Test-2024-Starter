b1d3ab73470ac6b07b524f6f939e2d28477ab547
Medo1902 <C20338661@mytudublin.ie>
2024-04-23 09:42:44 +0100

    added fields

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -1,6 +1,11 @@
 @tool
 extends Node3D
 
+@export var lenght = 0.0
+@export var frequency = 0.0
+@export var start_angle = 0.0
+@export var base_size = 0.0
+@export var multiplier = 0.0
 
 func _process(delta):
 	pass		
